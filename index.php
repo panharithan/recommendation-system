d68b991eba6454c441f004ac58acf816dfd604e9
An Panharith <an.panharith111@gmail.com>
2025-01-21 11:12:38 +0300

    add developer name

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ include 'header.php';  // Include the header
 ?>
 
 <div class="container">
-    <h1 class="mt-5 text-center">Welcome to Recommendation Management System!</h1>
+    <h1 class="mt-5 text-center">Reference Letter Submission System</h1>
+    
     <p class="lead text-center">Please log in to access your dashboard, or register if you're a new user.</p>
     
     <div class="d-flex justify-content-center">
@@ -13,5 +14,10 @@ include 'header.php';  // Include the header
         <a href="register.php" class="btn btn-secondary mx-2">Register</a>
     </div>
 </div>
+
+<footer class="text-center mt-4">
+    <p class="mb-0">Developed by <a href="https://github.com/panharithan" target="_blank" > Panharith An </a>, 2025, in CyberMACS programme</p>
+</footer>
+
 
 <?php include 'footer.php'; // Include the footer ?>
